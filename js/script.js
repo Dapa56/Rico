ceae69e2a171bebcb4e35d7897e9f8fd316b6733
Dapa56 <dapaputra592@gmail.com>
2023-07-19 21:55:36 +0800

    Rico

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika humberger menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik dluar sidebar untuk menghilangkan nav
+
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
